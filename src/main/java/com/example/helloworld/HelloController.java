03a6d891c34ed6deb98ee84c2c19a8b7e5cbb2a6
Vinoth Chinnakonda <vinoth@Vinoths-MacBook-Air.local>
2023-01-16 00:50:26 -0500

    added get mapping annotation

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.helloworld;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
 
+    @GetMapping
     public String hello(){
         return "Hello Vinoth";
     }
