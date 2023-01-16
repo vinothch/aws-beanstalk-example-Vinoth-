faae19c90960fcb81833fbf086d63b13bb47b6d1
Vinoth Chinnakonda <vinoth@Vinoths-MacBook-Air.local>
2023-01-15 21:09:18 -0500

    aws beanstalk example

---
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    public String hello(){
+        return "Hello Vinoth";
+    }
+
+}
